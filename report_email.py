1f860d927afd5c06cfa169d13a6e6c8f2f678ecc
SCXIV <Scarlettcoding@gmail.com>
2023-06-16 12:17:22 -0400

    added code for summary and sending email

--- report_email.py
+++ report_email.py
@@ -21,3 +21,16 @@ for filename in os.listdir(directory):
             if lbs in text:
                 fruit_weight = "weight: " + text
                 weights.append(fruit_weight)
+
+given_summary = ""
+for name, weight in zip(names, weights):
+    given_summary += name + '<br />' + weight + '<br />' + '<br />'
+
+if __name__ == "__main__":
+    reports.generate_report("/tmp/processed.pdf", date_string, given_summary)
+    sender = "automation@example.com"
+    receiver = "STUDENTIP@example.com"
+    subject = "Upload Completed - Online Fruit Store"
+    body = "All fruits are uploaded to our website successfully. A detailed list is attached to this email."
+    message = emails.generate_email(sender, receiver, subject, body, "/tmp/processed.pdf")
+    emails.send_email(message)
