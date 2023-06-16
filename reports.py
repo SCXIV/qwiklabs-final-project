52675989899520945c424a21a3c348456a73d385
SCXIV <Scarlettcoding@gmail.com>
2023-06-16 11:25:48 -0400

    added reports.py

---
+++ reports.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Table, Image
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib import colors
