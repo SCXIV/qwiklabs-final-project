b9fbe0bc3a0b4746b3dd7b1a0aeebf026b9fcbc0
SCXIV <Scarlettcoding@gmail.com>
2023-06-16 10:47:40 -0400

    finished code for supplier_image_upload script

--- supplier_image_upload.py
+++ supplier_image_upload.py
@@ -3,4 +3,11 @@
 import requests
 import os
 
-url = "http://localhost/upload"
+url = "http://CHANGENAMETOIPXXXXX/upload" #PLACEHOLDER FOR LAB IP
+directory = "./supplier-data/images/"
+
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    if f.endswith(".jpeg"):
+        with open(f, 'rb') as opened_file:
+            r = requests.post(url, files = {'file': opened_file})
