2d09a4cae6e953ec04ddd021c3b7f5183c04bf90
SCXIV <Scarlettcoding@gmail.com>
2023-06-16 11:40:54 -0400

    added code for reports.py

--- reports.py
+++ reports.py
@@ -3,3 +3,13 @@
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Table, Image
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.lib import colors
+
+def generate_report(attachment, title, paragraph):
+    styles = getSampleStyleSheet()
+    report = SimpleDocTemplate(attachment)
+    report_title = Paragraph(title, styles["h1"])
+    report_info = Paragraph(paragraph, styles["BodyText"])
+    table_style = [('GRID', (0, 0), (-1, -1), 1, colors.black), ('FONTNAME', (0, 0), (-1, 0), 'Arial'), ('ALIGN', (0, 0), (-1, -1), 'CENTER')]
+    line_break = Spacer(1, 20)
+
+    report.build([report_title, line_break, report_info])
