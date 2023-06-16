37c087eec7dbaa23a1920ba50e0bde6d1c7b68e8
SCXIV <Scarlettcoding@gmail.com>
2023-06-16 11:59:12 -0400

    finished first for loop in report_email.py

--- report_email.py
+++ report_email.py
@@ -10,3 +10,14 @@ date_string = "Processed Update on " + date_time
 names = []
 weights = []
 directory = "./suppliet-data/descriptions/"
+
+for filename in os.listdir(directory):
+    with open(directory + filename) as f:
+        for text in f:
+            trimmed_text = text.strip()
+            if len(text) <= 10 and len(text) > 0  and "lb" not in text:
+                fruit_name = "name: " + text
+                names.append(fruit_name)
+            if lbs in text:
+                fruit_weight = "weight: " + text
+                weights.append(fruit_weight)
